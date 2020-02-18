0d483b3f1b9e7d9e80f7210f59e73d9998eabdf5
jerald-jacob <jeraldjacob10@gmail.com>
2020-02-18 17:28:55 +0530

    created new flutter app for getting the current location

---
+++ flutter_google_map/lib/main.dart
@@ -0,0 +1,198 @@
+//import 'dart:async';
+//import 'package:location/location.dart';
+//import 'package:flutter/material.dart';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Google Maps Demo',
+//      home: MapSample(),
+//    );
+//  }
+//}
+//
+//class MapSample extends StatefulWidget {
+//  @override
+//  State<MapSample> createState() => MapSampleState();
+//}
+//
+//class MapSampleState extends State<MapSample> {
+//  Completer<GoogleMapController> _controller = Completer();
+//
+//  static final CameraPosition _kGooglePlex = CameraPosition(
+//    target: LatLng(37.42796133580664, -122.085749655962),
+//    zoom: 14.4746,
+//  );
+//
+//  static final CameraPosition _kLake = CameraPosition(
+//      bearing: 192.8334901395799,
+//      target: LatLng(37.43296265331129, -122.08832357078792),
+//      tilt: 59.440717697143555,
+//      zoom: 19.151926040649414);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: AppBar(
+//        title: Text("Google Map"),
+//      ),
+//      body: GoogleMap(
+//        mapType: MapType.hybrid,
+//        initialCameraPosition: _kGooglePlex,
+////        markers: Set.of((marker !=null)? [marker]:[]),
+//        onMapCreated: (GoogleMapController controller) {
+//          _controller.complete(controller);
+//        },
+//      ),
+//      floatingActionButton: FloatingActionButton.extended(
+//        onPressed: _goToTheLake,
+//        label: Text('To the lake!'),
+//        icon: Icon(Icons.location_searching),
+//      ),
+//    );
+//  }
+//
+//  Future<void> _goToTheLake() async {
+//    final GoogleMapController controller = await _controller.future;
+//    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+//  }
+//}
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hakodate morning market',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Hakodate morning market'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamSubscription _locationSubscription;
+  Location _locationTracker = Location();
+  Marker marker;
+  Circle circle;
+  GoogleMapController _controller;
+
+  static final CameraPosition initialLocation = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<Uint8List> getMarker() async {
+    ByteData byteData = await DefaultAssetBundle.of(context).load("assets/car_icon.png");
+    return byteData.buffer.asUint8List();
+  }
+
+  void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
+    LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
+    this.setState(() {
+      marker = Marker(
+          markerId: MarkerId("home"),
+          position: latlng,
+          rotation: newLocalData.heading,
+          draggable: true,
+          zIndex: 2,
+          flat: true,
+          anchor: Offset(0.5, 0.5),
+          icon: BitmapDescriptor.fromBytes(imageData));
+      circle = Circle(
+          circleId: CircleId("car"),
+          radius: newLocalData.accuracy,
+          zIndex: 1,
+          strokeColor: Colors.blue,
+          center: latlng,
+          fillColor: Colors.blue.withAlpha(70));
+    });
+  }
+
+  void getCurrentLocation() async {
+    try {
+
+      Uint8List imageData = await getMarker();
+      var location = await _locationTracker.getLocation();
+print(location.latitude);
+print(location.longitude);
+      updateMarkerAndCircle(location, imageData);
+
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+
+      _locationSubscription = _locationTracker.onLocationChanged().listen((newLocalData) {
+        if (_controller != null) {
+          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+              bearing: 192.8334901395799,
+              //target: LatLng(newLocalData.latitude, newLocalData.longitude),
+              target: LatLng(10.0042088, 76.3755233),
+              tilt: 0,
+              zoom: 18.00)));
+          updateMarkerAndCircle(newLocalData, imageData);
+        }
+      });
+
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialLocation,
+        markers: Set.of((marker != null) ? [marker] : []),
+        circles: Set.of((circle != null) ? [circle] : []),
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.location_searching),
+          onPressed: () {
+            getCurrentLocation();
+          }),
+    );
+  }
+}
